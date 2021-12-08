2256c9a1df9b243c68b1e77f049b46b4174ae11c
KareemAlaa2001 <kareemalaa2001@gmail.com>
2021-12-08 15:02:07 +0000

    added script for high LR bn only run

---
+++ run_bn_high_lr_experiment.sh
@@ -0,0 +1,1 @@
+python pytorch_mlp_framework/train_evaluate_image_classification_system.py --batch_size 100 --seed 0 --num_filters 32 --num_stages 3 --num_blocks_per_stage 5 --experiment_name BN_Experiment_Updated_HIGH_LR --use_gpu True --num_classes 100 --block_type 'bn_block' --learning_rate 1e-2 --continue_from_epoch -1 
